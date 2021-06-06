16b70220ea6fbb044f0c5d942309f2b76abe943d
Lyon <46277338+yannnnLi@users.noreply.github.com>
2021-06-06 12:22:34 +0800

    Create zzz_merge.py

---
+++ zzz_merge.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+
+f = sys.argv[2:]
+df = pd.read_table(sys.argv[1],sep="\t")
+for i in f:
+    df1 = pd.read_table(i,sep="\t")
+    df = pd.merge(df,df1,how="inner",on="peptide")
+
+df.to_csv("zzz_368.txt",sep="\t",index=None)
