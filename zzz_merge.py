9cce950949eb7b038c95aa542c5c04b3aa68e7fd
Lyon <46277338+yannnnLi@users.noreply.github.com>
2021-06-06 12:26:05 +0800

    Update zzz_merge.py

--- zzz_merge.py
+++ zzz_merge.py
@@ -5,6 +5,6 @@ f = sys.argv[2:]
 df = pd.read_table(sys.argv[1],sep="\t")
 for i in f:
     df1 = pd.read_table(i,sep="\t")
-    df = pd.merge(df,df1,how="inner",on="peptide")
+    df = pd.merge(df,df1,how="inner",on="gene")
 
 df.to_csv("zzz_368.txt",sep="\t",index=None)
